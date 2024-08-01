53b1877a70312973ce659328562bb00670c46e73
Martin Brocker <brocker.martin@gmail.com>
2024-08-01 16:16:33 +0200

    Add Material UI framework
    Setting the basic Material UI theme framework for use in the project.
    Added a basic Landing page and a custom 404 page.
    
    86bztm8x5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
+
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <Header />
+              {children}
+            <Footer />
+          </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import Box from '@mui/material/Box';
+import { Container, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Container maxWidth={false}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        paddingTop={8}
+        paddingBottom={8}
+      >
+        <Typography variant="h2" align="center" gutterBottom>
+          4 0 4
+        </Typography>
+        <Typography variant="h4" align="center" gutterBottom>
+          The page you are looking for could not be found
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
