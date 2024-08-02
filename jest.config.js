60978ad8f9775b9d1ed5157e3e90fb801e6bb0fe
Martin Brocker <brocker.martin@gmail.com>
2024-08-02 13:46:47 +0200

    Add CodeCoverage check
    As part of the CICD check, ensure the code coverage hasnt dropped
    
    86bztm8x5

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
-
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  // transform: {
-  //   '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
-  // }
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/{!(*.d.ts),}.{ts,js,tsx,jsx}"]
 };
