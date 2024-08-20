a75f66c092d827e9e16e8e73315f35d19243af8f
Martin Brocker <brocker.martin@gmail.com>
2024-08-20 12:15:35 +0200

    Minor cleanup
    Remove Search icon

--- src/app/patient-summary-viewer/page.tsx
+++ src/app/patient-summary-viewer/page.tsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box';
 import { Container, Typography } from "@mui/material";
-import Search from '@mui/icons-material/Search';
 
 import FhirBundleViewer from './components/FHIRBundleViewer'; // Adjust path as needed
 import fhirBundleJson from './_sample/bundle.json'; // Your FHIR Bundle JSON
@@ -16,7 +15,6 @@ export default function PatientSummaryViewer() {
         paddingTop={4}
         paddingBottom={4}
       >
-        <Search sx={{fontSize: 40}} /> 
         <Typography variant="h4" align="center" gutterBottom>
           Patient Summary Viewer: Patient: John Snow
         </Typography>
