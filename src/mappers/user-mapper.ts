dd0294549fd8f94603352706b92d30eb89482dbf
jacob-khoza-symb <157815500+jacob-khoza-symb@users.noreply.github.com>
2024-08-22 17:25:57 +0200

    Resolving mapping bug.

--- src/mappers/user-mapper.ts
+++ src/mappers/user-mapper.ts
@@ -2,33 +2,33 @@ import { UserDto } from "@/domain/dtos/user";
 import { UserModel } from "@/domain/models/user";
 import { UserEntity } from "@/entities/user";
 
-export const mapEntityToModel = (userEntity: UserEntity): UserModel => {
-    return userEntity && new UserModel(
+export const mapEntityToModel = (userEntity: UserEntity): UserModel | undefined => {
+    return userEntity ? new UserModel(
         userEntity.user_id, 
         userEntity.patient_id, 
         userEntity.id
-    );
+    ) : undefined;
 }
 
-const mapModelToEntity = (userModel: UserModel): UserEntity => {
-    return userModel && {
+const mapModelToEntity = (userModel: UserModel): UserEntity | undefined => {
+    return userModel ? {
         id: userModel.getId(),
         user_id: userModel.getUserId(),
         patient_id: userModel.getPatientId()
-    };
+    } : undefined;
 }
 
-export const mapModelToDto = (userModel: UserModel): UserDto => {
+export const mapModelToDto = (userModel: UserModel): UserDto | undefined => {
 
-    return userModel && {
+    return userModel ? {
         id: userModel.getId(),
-        patientId: userModel.getId(),
-        userId: userModel.getId()
-    };
+        patientId: userModel.getPatientId(),
+        userId: userModel.getUserId()
+    } : undefined;
 }
 
-export const mapDtoToModel = (userDto: UserDto): UserModel => {
-    return userDto && new UserModel(
+export const mapDtoToModel = (userDto: UserDto): UserModel | undefined => {
+    return userDto ? new UserModel(
         userDto.userId, userDto.patientId, userDto.id
-    );
+    ) : undefined;
 }
--- src/usecases/users/get-user.ts
+++ src/usecases/users/get-user.ts
@@ -1,10 +1,10 @@
 import { UserModel } from "@/domain/models/user"
-import prisma from "@/infrastructure/clients/prisma";
 import { UserPrismaRepository } from "@/infrastructure/repositories/prisma/user-repository"
 import { mapEntityToModel } from "@/mappers/user-mapper";
 
 export const getUserUseCase = async (context: {repo: UserPrismaRepository}, data: {id: string}): Promise<UserModel> => {
     const entity = await context.repo.findById(data.id);
+    console.log(entity);
     return mapEntityToModel(entity);
 }
 
