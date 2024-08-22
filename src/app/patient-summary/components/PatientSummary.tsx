fdf4cab1619047d6184e56d4430caead59aee8b5
amalessid <amal.essid@esprit.tn>
2024-08-22 12:02:38 +0100

    Add tab rendering logic based on fhir sample

--- src/app/patient-summary/components/PatientSummary.tsx
+++ src/app/patient-summary/components/PatientSummary.tsx
@@ -1,10 +1,46 @@
+"use client";
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
+import fhirBundleJson from "../sample/bundle.json"
+import TabPanel from "./TabPanel";
+import { useState } from "react";
+
+const dataTabs = Array.from(
+  new Set(fhirBundleJson.entry.map((entry) => entry.resource.resourceType))
+);
+
+const renderTabs = () =>
+  dataTabs.map((resourceType) => (
+    <Tab label={resourceType} key={resourceType} value={resourceType} />
+  ));
 
 export default function PatientSummary() {
+  const [selectedTab, setSelectedTab] = useState(dataTabs[0]);
+  const renderPanels = () =>
+    dataTabs.map((resourceType) => {
+      return (
+        <TabPanel value={resourceType} index={selectedTab} key={resourceType}>
+            <div>
+              Data of <b>{resourceType}</b> will be displayed here.
+            </div>
+        </TabPanel>
+      );
+    });
+  const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
+    setSelectedTab(newValue);
+  };
+
   return (
-    <Typography variant="h4" align="center" gutterBottom>
-      Welcome to patient Summary page ..
-    </Typography>
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={selectedTab} onChange={handleChange} variant="scrollable" scrollButtons="auto"
+        >
+          {renderTabs()}
+        </Tabs>
+      </Box>
+      {renderPanels()}
+    </Box>
   );
 }
