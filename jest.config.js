7138d919c9e36142a0bf88ffeceef5a995a799c5
Amaechi Chisom <amaechilegend@gmail.com>
2024-08-23 10:55:45 +0100

    updated jest.config.js script and added testcases to user-mapper.test.ts

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   modulePathIgnorePatterns: ['<rootDir>/.next'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   },
