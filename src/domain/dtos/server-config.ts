012d911a84cdfc20ceb083adae65ef6665cb2fb1
jacob-khoza-symb <157815500+jacob-khoza-symb@users.noreply.github.com>
2024-08-27 07:58:02 +0200

    Added functionality to get server configs.

--- src/domain/dtos/server-config.ts
+++ src/domain/dtos/server-config.ts
@@ -3,7 +3,7 @@ export class CreateServerConfigDto {
     refreshToken?: string;
     refreshTime?: Date;
     tokenEndpoint?: string;
-    endpointUrl?: string;
+    endpointUrl: string;
     clientSecret?: string;
     clientId?: string;
     accessTokenResponse?: string
