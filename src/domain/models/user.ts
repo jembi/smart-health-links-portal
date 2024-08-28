90e2f93dc8639ea27bc0f8024e730521a83d43f8
jacob-khoza-symb <157815500+jacob-khoza-symb@users.noreply.github.com>
2024-08-28 09:44:57 +0200

    Added basic logic for retrieving patient data.

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -6,7 +6,7 @@ export class UserModel extends BaseModel{
     constructor(private userId: string, private patientId: string, private id?: string){
         super(z.object({
             userId: z.string().min(10),
-            patientId: z.string().min(2),
+            patientId: z.string().min(1),
             id: z.string().optional()
         }));
         this.validate();
