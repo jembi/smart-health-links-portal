65557df7c56ecae614ad49e028410bc8eac4cec9
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2024-08-28 18:08:25 +0100

    feat: initial keycloak integration

---
+++ src/app/components/Logout.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { Button, Typography } from "@mui/material";
+import { signOut } from "next-auth/react";
+export default function Logout() {
+  return <Button onClick={() => signOut()} variant="contained" color="secondary">
+    <Typography>Signout of keycloak</Typography>
+  </Button>
+}
