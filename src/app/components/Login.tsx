63d72741711480317307410dddcdfd7dc3212776
hexastack <talebhassan.ikbel@gmail.com>
2024-08-29 09:53:33 +0100

    enhance export and configs

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -3,6 +3,6 @@ import { Button } from "@mui/material";
 import { signIn } from "next-auth/react";
 export default function Login() {
   return <Button onClick={() => signIn("keycloak")} variant="contained" color="secondary">
-    Signin with keycloak
+    Sign in
   </Button>
 }
--- src/app/components/Logout.tsx
+++ src/app/components/Logout.tsx
@@ -3,6 +3,6 @@ import { Button, Typography } from "@mui/material";
 import { signOut } from "next-auth/react";
 export default function Logout() {
   return <Button onClick={() => signOut()} variant="contained" color="secondary">
-    <Typography>Signout of keycloak</Typography>
+    <Typography>Sign out</Typography>
   </Button>
 }
