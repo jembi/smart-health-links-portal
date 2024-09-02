57d2fbf1a89b11f20bfd6fe789c620493ad96581
jacob-khoza-symb <157815500+jacob-khoza-symb@users.noreply.github.com>
2024-09-02 16:34:10 +0200

    Resolving merge conflicts.

--- src/app/api/v1/share-link/[id]/route.ts
+++ src/app/api/v1/share-link/[id]/route.ts
@@ -1,12 +1,12 @@
 import { NOT_FOUND } from "@/app/constants/http-constants";
 import { handleApiValidationError } from "@/app/utils/error-handler";
-import prisma from "@/infrastructure/clients/prisma";
-import { SHLinkPrismaRepository } from "@/infrastructure/repositories/prisma/shlink-repository";
+import { container, SHLinkRepositoryToken } from "@/container";
+import { ISHLinkRepository } from "@/infrastructure/repositories/interfaces/shlink-repository";
 import { mapModelToDto } from "@/mappers/shlink-mapper";
 import { getSingleSHLinkUseCase } from "@/usecases/shlinks/get-single-shlink";
 import { NextResponse } from "next/server";
 
-const repo = new SHLinkPrismaRepository(prisma);
+const repo = container.get<ISHLinkRepository>(SHLinkRepositoryToken);
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
 
--- src/container.ts
+++ src/container.ts
@@ -1,10 +1,11 @@
-import { PrismaClient } from '@prisma/client';
 import { ServerConfigPrismaRepository } from './infrastructure/repositories/prisma/server-config-repository';
 import { UserPrismaRepository } from './infrastructure/repositories/prisma/user-repository';
 import prisma from './infrastructure/clients/prisma';
+import { SHLinkPrismaRepository } from './infrastructure/repositories/prisma/shlink-repository';
 
 export const ServerConfigRepositoryToken = Symbol('ServerConfigPrismaRepository');
 export const UserRepositoryToken = Symbol('UserPrismaRepository');
+export const SHLinkRepositoryToken = Symbol('SHLinkRepository');
 
 export class Container {
     private services = new Map<symbol, any>();
@@ -12,6 +13,7 @@ export class Container {
     constructor() {
         this.services.set(ServerConfigRepositoryToken, new ServerConfigPrismaRepository(prisma));
         this.services.set(UserRepositoryToken, new UserPrismaRepository(prisma));
+        this.services.set(SHLinkRepositoryToken, new SHLinkPrismaRepository(prisma));
     }
 
     public get<T>(token: symbol): T {
