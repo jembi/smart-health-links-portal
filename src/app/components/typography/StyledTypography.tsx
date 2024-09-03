1d25a235b9c10c21319c44a07f41ecaa200aa721
amalessid <amal.essid@esprit.tn>
2024-09-03 11:34:23 +0100

    address PR review: switch from px to rem unit

--- src/app/components/typography/StyledTypography.tsx
+++ src/app/components/typography/StyledTypography.tsx
@@ -2,10 +2,9 @@ import { styled, Typography } from "@mui/material";
 
 export const StyledSectionTypography = styled(Typography)({
   backgroundImage: "linear-gradient(270deg, hsla(0, 0%, 86.3%, .05), #e6e6e6)",
-  marginTop: "10px",
-  marginBottom: "10px",
+  marginBlock: ".62rem",
 });
 
 export const StyledTableCaptionTypography = styled(Typography)({
-  marginRight: "0.5",
+  marginRight: ".03rem",
 });
