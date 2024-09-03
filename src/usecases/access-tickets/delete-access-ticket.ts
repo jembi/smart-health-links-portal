bf22d635419dfd96dc3c95ec3a3ecbddcf250068
jacob-khoza-symb <157815500+jacob-khoza-symb@users.noreply.github.com>
2024-09-03 12:02:03 +0200

    Added delete use case.

---
+++ src/usecases/access-tickets/delete-access-ticket.ts
@@ -0,0 +1,12 @@
+import { AccessTicketModel } from "@/domain/models/access-ticket";
+import { IAccessTicketRepository } from "@/infrastructure/repositories/interfaces/access-ticket-repository.interface";
+import { mapEntityToModel } from "@/mappers/access-ticket-mapper";
+
+export const deleteAccessTicketUseCase = async (context: {repo: IAccessTicketRepository}, data: {id: string}): Promise<AccessTicketModel | undefined> => {
+    const entity = await context.repo.findById(data.id);
+
+    if(!entity) return;
+
+    await context.repo.delete(entity);
+    return mapEntityToModel(entity);
+}
