f600b57d2d4cc2156925b2a9fb1987d71069a675
Amaechi Chisom <amaechilegend@gmail.com>
2024-09-05 10:00:57 +0100

    resolving code rabbit issues

--- src/app/api/v1/share-links/[id]/endpoints/[endpointId]/route.ts
+++ src/app/api/v1/share-links/[id]/endpoints/[endpointId]/route.ts
@@ -27,25 +27,29 @@ export async function GET(
   const url = new URL(request.url);
   const ticketId = url.searchParams.get('ticket');
   
-  const ticket:AccessTicketModel = await getAccessTicketUseCase({repo:ticketRepo}, ticketId)
+  try{
+    
+    const ticket:AccessTicketModel = await getAccessTicketUseCase({repo:ticketRepo}, ticketId)
 
-  if (!ticket) {
-    return NextResponse.json({message:UNAUTHORIZED_REQUEST}, { status: 401 });
-  }
-  
-  if (ticket?.getSHLinkId() !== params.id) {
-    return NextResponse.json({message:NOT_FOUND}, { status: 404 });
-  }   
-  
-  const shlink = await getSingleSHLinkUseCase({ repo:shlinkRepo}, { id: params.id });
+    if (!ticket) {
+      return NextResponse.json({message:UNAUTHORIZED_REQUEST}, { status: 401 });
+    }
+    
+    if (ticket.getSHLinkId() !== params.id) {
+      return NextResponse.json({message:NOT_FOUND}, { status: 404 });
+    }   
+    
+    const shlink = await getSingleSHLinkUseCase({ repo:shlinkRepo}, { id: params.id });
 
-  const user = await getUserUseCase({ repo:userRepo }, { userId: shlink.getUserId() });
-  
-  const patient = await getPatientDataUseCase({ repo:serverConfigRepo }, { user: user });
+    const user = await getUserUseCase({ repo:userRepo }, { userId: shlink.getUserId() });
+    
+    const patient = await getPatientDataUseCase({ repo:serverConfigRepo }, { user: user });
 
-  if(patient){
     return NextResponse.json(patient, { status: 200 });
   }
+  catch{
+    return NextResponse.json({ message: NOT_FOUND }, { status: 404 });
+  }
 
-  return NextResponse.json({ message: NOT_FOUND }, { status: 404 });
+  
 }
