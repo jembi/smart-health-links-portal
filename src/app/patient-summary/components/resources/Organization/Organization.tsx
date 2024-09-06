7cb503de1a4cdc93151ec1782bd2b001473637fd
amalessid <amal.essid@esprit.tn>
2024-09-06 08:22:22 +0100

    minor: add key to the top level fragment

--- src/app/patient-summary/components/resources/Organization/Organization.tsx
+++ src/app/patient-summary/components/resources/Organization/Organization.tsx
@@ -2,18 +2,19 @@ import { StyledSectionTypography } from '@/app/components/typography/StyledTypog
 import InfoRow from '../../InfoRow';
 import ConnectionDetails from '../Patient/ConnectionDetails';
 import { Organization as OrganizationType } from '@/app/patient-summary/types/resources/organisation.types';
+import React from 'react';
 
 export default function Organization({ data }: { data: OrganizationType[] }) {
   if (!Array.isArray(data) || data.length === 0) {
     return null;
   }
   return data.map((organizationInfo, index) => (
-    <>
+    <React.Fragment key={index}>
       <StyledSectionTypography key={index}>
         Organization ({index + 1})
       </StyledSectionTypography>
       <InfoRow label="Name" value={organizationInfo.name} />
       <ConnectionDetails resourceInfo={organizationInfo} />
-    </>
+    </React.Fragment>
   ));
 }
