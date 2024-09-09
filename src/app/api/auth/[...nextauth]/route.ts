8843e1e9a9e5991e784676f726746df85476d438
Martin Brocker <brocker.martin@gmail.com>
2024-09-09 10:29:32 +0200

    Fix broken Production build
    There were some issues noted when running yarn build to create a production build images
    These changes fixes the issues to allow successful production build to happen

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,6 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import KeycloakProvider from "next-auth/providers/keycloak";
+import NextAuth from "next-auth";
+import { authOptions } from "../authOptions";
 
-export const authOptions: AuthOptions = {
-  providers: [
-    KeycloakProvider({
-      jwks_endpoint: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/shlp/protocol/openid-connect/certs`,
-      wellKnown: undefined,
-      clientId: process.env.KEYCLOAK_CLIENT_ID,
-      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
-      issuer: process.env.KEYCLOAK_ISSUER,
-      authorization: {
-        params: {
-          scope: "openid email profile",
-        },
-        url: `${process.env.NEXT_LOCAL_KEYCLOAK_URL}/realms/shlp/protocol/openid-connect/auth`,
-      },
-      token: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/shlp/protocol/openid-connect/token`,
-      userinfo: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/shlp/protocol/openid-connect/userinfo`,
-    }),
-  ],
-};
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
---
+++ src/app/api/auth/authOptions.ts
@@ -0,0 +1,22 @@
+import { AuthOptions } from "next-auth";
+import KeycloakProvider from "next-auth/providers/keycloak";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    KeycloakProvider({
+      jwks_endpoint: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/shlp/protocol/openid-connect/certs`,
+      wellKnown: undefined,
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET,
+      issuer: process.env.KEYCLOAK_ISSUER,
+      authorization: {
+        params: {
+          scope: "openid email profile",
+        },
+        url: `${process.env.NEXT_LOCAL_KEYCLOAK_URL}/realms/shlp/protocol/openid-connect/auth`,
+      },
+      token: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/shlp/protocol/openid-connect/token`,
+      userinfo: `${process.env.NEXT_CONTAINER_KEYCLOAK_ENDPOINT}/realms/shlp/protocol/openid-connect/userinfo`,
+    }),
+  ],
+};
