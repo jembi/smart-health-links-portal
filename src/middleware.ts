8d2eb370ee546fd0d72e6ead31391d8b57786dce
jacob-khoza-symb <157815500+jacob-khoza-symb@users.noreply.github.com>
2024-09-10 08:52:47 +0200

    Resolving PR comments.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,14 @@ import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(req: Request) {
-  const token =  await getToken({req: req as any}) //getAuthToken(req);
-  
   try{
-    //const result = validateToken(token);
-    console.log(token);
-    if(!token) throw 'result';
+    const token =  await getToken({req: req as any});
+    if(!token) throw new Error();
     
     return NextResponse.next();
   }
   catch(error){
-    console.log(error);
-    return NextResponse.json({message: 'Invalid Token'}, { status: 422 });
+    return NextResponse.json({message: 'Invalid token or token expired'}, { status: 401 });
   }
 }
 
