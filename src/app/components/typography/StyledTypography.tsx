763cc349c26a909986de8c39348290fac8b86f2b
yassinedorbozgithub <yassine.dorboz.2022@gmail.com>
2024-09-16 08:17:01 +0100

    Update: remove arrow function

--- src/app/components/typography/StyledTypography.tsx
+++ src/app/components/typography/StyledTypography.tsx
@@ -1,12 +1,12 @@
 import { styled, Typography } from '@mui/material';
 
-export const StyledSectionTypography = styled(Typography)(() => ({
+export const StyledSectionTypography = styled(Typography)({
   backgroundImage: 'linear-gradient(270deg, hsla(0, 0%, 86.3%, .05), #e6e6e6)',
   marginBlock: '.62rem',
   fontWeight: 700,
   fontSize: '1.1em',
   width: '100%',
-}));
+});
 
 export const StyledTableCaptionTypography = styled(Typography)({
   marginRight: '.03rem',
