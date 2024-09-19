ebc06613c4e422dc222aedec47f530ca8256e5b7
jacob-khoza-symb <157815500+jacob-khoza-symb@users.noreply.github.com>
2024-09-19 18:10:05 +0200

    Removing debug code.

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,3 @@
-import { unstable_noStore } from 'next/cache';
 import { NextResponse, NextRequest } from 'next/server';
 
 import { LogHandler } from '@/lib/logger';
@@ -6,10 +5,6 @@ import { LogHandler } from '@/lib/logger';
 const logger = new LogHandler(__dirname);
 
 export async function GET(request) {
-  unstable_noStore();
   logger.info('API connected successfully');
-  return NextResponse.json(
-    { message: 'API Health Check', env: process.env.POSTGRES_PRISMA_URL },
-    { status: 200 },
-  );
+  return NextResponse.json({ message: 'API Health Check' }, { status: 200 });
 }
