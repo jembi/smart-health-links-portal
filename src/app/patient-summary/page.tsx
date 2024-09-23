017ae4b35a741ebecfd9c36b8dab5d44c11093c3
Mohamed Chedli <chedli@symbionix.co>
2024-09-23 21:43:36 +0100

    feat: Integrate Patient Summary view with API

--- src/app/patient-summary/page.tsx
+++ src/app/patient-summary/page.tsx
@@ -1,12 +1,44 @@
+'use client';
+import { useEffect, useState } from 'react';
 import { Container } from '@mui/material';
 import Box from '@mui/material/Box';
-
+import { useAuth } from '../context/AuthProvider';
+import { apiSharedLink } from '../utils/api.class';
 import { TBundle } from '@/types/fhir.types';
 
 import PatientSummary from './components/PatientSummary';
-import fhirBundleJson from './sample/bundle.json';
+import PatientSummarySkeleton from './PatientSummarySkeleton';
+import ErrorState from './ErrorState';
 
 export default function PatientSummaryPage() {
+  const { user, isAuthenticated } = useAuth();
+  const [fhirBundle, setFhirBundle] = useState<TBundle | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiSharedLink.getPatientData(user.id);
+        setFhirBundle(response.data as TBundle);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [isAuthenticated, user]);
+
+  if (loading) {
+    return <PatientSummarySkeleton />;
+  }
+
+  if (error) {
+    return <ErrorState message={error} />;
+  }
+
   return (
     <Container maxWidth={false}>
       <Box
@@ -17,7 +49,7 @@ export default function PatientSummaryPage() {
         paddingTop={8}
         paddingBottom={8}
       >
-        <PatientSummary fhirBundle={fhirBundleJson as TBundle} />
+        {fhirBundle && <PatientSummary fhirBundle={fhirBundle} />}
       </Box>
     </Container>
   );
