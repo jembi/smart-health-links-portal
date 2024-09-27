fcd7927824f4bcde4a099859a7890ce6f73667f4
Amaechi Chisom <amaechilegend@gmail.com>
2024-09-27 15:45:11 +0100

    migration files restored

--- prisma/migrations/20240927084525_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `deleted_at` on the `shlink` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "shlink" DROP COLUMN "deleted_at";
