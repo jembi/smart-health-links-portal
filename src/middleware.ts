0478ce635ba45ff9bc592fc089b0687985c8e7b4
jacob-khoza-symb <157815500+jacob-khoza-symb@users.noreply.github.com>
2024-09-27 12:26:10 +0200

    Removed authentication for get single share link endpoints. Added check for tokenid on get share-link-endpoint endpoint. Updated tests.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,18 +3,32 @@ import { getToken } from 'next-auth/jwt';
 
 const FRONTEND_PATHNAMES = ['/', '/patient-summary', '/shared-links'];
 const REDIRECTION_PATHNAME = '/';
+const IGNORE_PATHS = {
+  POST: [/^\/api\/v1\/share-links\/([^/]+)/],
+  GET: [/^\/api\/v1\/share-links\/([^/]+)\/endpoints\/([^/]+)/],
+};
+
+type HTTP_VERB = 'GET' | 'POST' | 'DELETE' | 'PUT' | 'PATCH';
 
 const isApiPathname = (pathname: string) => pathname.startsWith('/api/v1/');
 const isFrontendPathname = (pathname: string) =>
   FRONTEND_PATHNAMES.includes(pathname);
 
+const isIgnorePath = (method: HTTP_VERB, pathname: string) => {
+  return !!IGNORE_PATHS[method]?.find((x) => pathname.match(x));
+};
+
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
-  const { nextUrl, url } = req;
+  const { nextUrl, url, method } = req;
   const { pathname } = nextUrl;
 
   try {
     if (!token) {
+      if (isIgnorePath(method as HTTP_VERB, pathname)) {
+        return NextResponse.next();
+      }
+
       if (isApiPathname(pathname)) throw new Error();
 
       if (pathname !== REDIRECTION_PATHNAME && isFrontendPathname(pathname)) {
